ba0f88153f04ff60b4aad227631a8d4c2f8afafe
Artem_Lakatun <alakatuni@mail.ru>
2023-09-25 01:06:40 +0300

    add: add html, css, js files feat: started working on the header(40%)

---
+++ index.js
@@ -0,0 +1,10 @@
+/* document.addEventListener("DOMContentLoaded", function () {
+    // Задержка перед началом закрытия черного экрана
+    setTimeout(() => {
+        const loader = document.querySelector('.loader');
+        loader.style.animation = "none"; // Отменяем анимацию и оставляем черный экран статичным
+        setTimeout(() => {
+            loader.style.animation = "slideUp 0.5s forwards"; // Запускаем анимацию закрытия снизу вверх
+        }, 1200); // 3 секунды (3000 миллисекунд)
+    }, 0);
+}); */
